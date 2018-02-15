a79fd08906bedbb7fe17afd75f24cd86594a88b6
Clayton Maksymiuk <claytonmaksymiuk@gmail.com>
2018-02-15 14:53:06 -0500

    Frozen Pixels
    Stop pixels from constantly updating randomly, comes in to play with
    randomized chunk pixels.

--- pygameTemplate.py
+++ pygameTemplate.py
@@ -3,14 +3,59 @@ import pygame
 import sys
 import time
 import random
+import csv
+
+##
+save = "testWorld"
+##
 
 colors = []
 for r in range(0,256,8):
     for g in range(0,256,8):
         for b in range(0,256,8):
             colors.append((r,g,b))
+           
+def init(save, l):
+    file = save + ".csv"
+    fw = open(file, 'w', newline='')
+    w = csv.writer(fw, dialect='excel')
+    w.writerows(l)
+    fw.close()
 
+def pregen_pixel(screen, save, x, y):
+    x-=(5*8)+5
+    y-=(5*8)+5
+    fill_area = []
+    for i in range(0,17):
+        for k in range(0,17):
+            fill_area.append((x+5*i,y+5*k))
+    file = save + ".csv"
+    fr = open(file, 'r')
+    r = csv.reader(fr)
+    biocolorsCSV = list(r)
+    biocolors = []
+    for i in biocolorsCSV:
+        rgb = []
+        for k in i:
+            rgb.append(int(k))
+        biocolors.append(rgb)
+    for i,k in enumerate(fill_area):
+        screen.fill(biocolors[i], (k, (5, 5)))
+    fr.close()
 
+def first_gen(screen, colors, x, y):
+    x-=(5*8)+5
+    y-=(5*8)+5
+    fill_area = []
+    for i in range(0,17):
+        for k in range(0,17):
+            fill_area.append((x+5*i,y+5*k))
+    tile = []
+    for i in fill_area:
+        color = random.choice(colors)
+        screen.fill(color, (i, (5, 5)))
+        tile.append(color)
+    init(save, tile)
 
 
 class TextPrint:
@@ -49,6 +94,9 @@ BLACK = (  0,   0,   0)
 clock = pygame.time.Clock()
 textPrint = TextPrint()
 
+
+first_gen(screen, colors, 600, 350)
+
 done = False
 while done==False:
     for event in pygame.event.get(): 
@@ -58,17 +106,9 @@ while done==False:
     textPrint.reset()
     ###
 
-    def pixel(screen, colors, x, y):
-        x-=(5*8)+5
-        y-=(5*8)+5
-        fill_area = []
-        for i in range(0,17):
-            for k in range(0,17):
-                fill_area.append((x+5*i,y+5*k))
-        for i in fill_area:
-            color = random.choice(colors)
-            screen.fill(color, (i, (5, 5)))
-    pixel(screen, colors, 600,350)
+    
+    pregen_pixel(screen, save, 600, 350)
+
     ###
     pygame.display.flip()
     clock.tick(20)
